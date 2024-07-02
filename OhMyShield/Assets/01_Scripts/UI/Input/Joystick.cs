4feabc89cf5bd22225b366c493389d5462cf0297
wingyu0897 <wingyu0897@gmail.com>
2024-07-02 16:30:03 +0900

    Joystick upgrade

--- OhMyShield/Assets/01_Scripts/UI/Input/Joystick.cs
+++ OhMyShield/Assets/01_Scripts/UI/Input/Joystick.cs
@@ -4,11 +4,22 @@ using UnityEngine.EventSystems;
 
 public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler 
 {
+	private Canvas _canvas;
+
 	[SerializeField] private RectTransform _boundsSpace;
 	[SerializeField] private RectTransform _handle;
 
-	[Space]
-	public UnityEvent OnValueChanged;
+	[Space] 
+	public UnityEvent<Vector2> OnValueChanged;
+
+	private Vector2 _value;
+	public Vector2 Value => _value;
+
+	private void Awake()
+	{
+		_canvas = _boundsSpace.root.GetComponent<Canvas>();
+		_value = Vector2.zero;
+	}
 
 	public void OnPointerDown(PointerEventData eventData)
 	{
@@ -17,22 +28,33 @@ public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
 
 	public void OnPointerUp(PointerEventData eventData)
 	{
-		
+		_handle.anchoredPosition = Vector2.zero;
+		_value = Vector2.zero;
 	}
 
 	public void OnDrag(PointerEventData eventData)
 	{
 		CalculateHandlePosition(eventData.position);
+		CalculateValue();
 	}
 
 	private void CalculateHandlePosition(Vector2 position)
 	{
-		print(Camera.main.ScreenToViewportPoint(position).ToString());
+		float scaleFactor = _canvas.scaleFactor;
+		float radius = _boundsSpace.rect.width * scaleFactor * 0.5f;
+
 		Vector2 direction = position - (Vector2)_boundsSpace.position;
-		if (direction.magnitude > _boundsSpace.rect.width * 0.5f)
+		if (direction.magnitude > radius)
 		{
-			direction = direction.normalized * _boundsSpace.rect.width * 0.5f;
+			direction = direction.normalized * radius;
 		}
-		_handle.anchoredPosition = direction;
+		_handle.anchoredPosition = direction / scaleFactor;
+	}
+
+	private void CalculateValue()
+	{
+		_value = _handle.anchoredPosition / (_boundsSpace.rect.width * 0.5f);
+
+		OnValueChanged?.Invoke(_value);
 	}
 }
